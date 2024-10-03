1e98a81c02a217a54028900e83e3bfc74ae2fe47
safda <safda@LAPTOP-97BJNMRO>
2024-10-03 15:58:45 +0530

    Added logger as info to show Application started message..

--- SpringBootPRJT/src/main/java/com/school/SpringBootPrjtApplication.java
+++ SpringBootPRJT/src/main/java/com/school/SpringBootPrjtApplication.java
@@ -3,12 +3,16 @@ package com.school;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class SpringBootPrjtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootPrjtApplication.class, args);
-		System.out.println("Applicaiton Started....");
+		log.info("AppStatus:--> Applicaiton started...");
+
 	}
 
 }
