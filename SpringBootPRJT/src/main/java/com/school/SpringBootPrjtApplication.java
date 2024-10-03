adb5c5fbfbd09cbc5c99091ae3fcd3d710812178
SAFDAR WAQAR <72647535+safdarwaqar@users.noreply.github.com>
2024-10-03 15:21:48 +0530

    Update SpringBootPrjtApplication.java
    Added log for application startup...

--- SpringBootPRJT/src/main/java/com/school/SpringBootPrjtApplication.java
+++ SpringBootPRJT/src/main/java/com/school/SpringBootPrjtApplication.java
@@ -8,6 +8,7 @@ public class SpringBootPrjtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootPrjtApplication.class, args);
+		System.out.println("Applicaiton Started...");
 	}
 
 }
